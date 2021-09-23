ee85403918b13ad130a54a97e50034b439f1b366
lugofranklin <lugofranklin@gmail.com>
2021-09-23 10:37:49 -0400

    Project Expanding Cards

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const panels = document.querySelectorAll('.panel');
+
+console.log(panels);
+
+
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeClassActive()
+        panel.classList.add('active')
+    });
+});
+
+function removeClassActive(){
+    panels.forEach(panel =>{
+        panel.classList.remove('active');
+    });
+}
